17fc1ca8cd5c4fd5709a2881b0f02eaca523ea4c
ftth_finland <76876959+Kreader-app@users.noreply.github.com>
2021-01-13 14:17:36 -0500

    Update dominate.js

--- src/lambda/dominate.js
+++ src/lambda/dominate.js
@@ -1,6 +1,6 @@
-exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hello World Domination!!!"})
-    };
-}
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    body: "Hello World Domination!!!"
+  });
+};
